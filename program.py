f13c0afcf993dc99b3c2ae073e53107cf4ee578b
Kunal <kunaldhir@live.in>
2014-02-18 00:06:19 +0530

    Create program.py

---
+++ program.py
@@ -0,0 +1,9 @@
+if Question == 'What is your name ?':
+    return 'My name is artificially intelligent' 
+if Question == 'What do you do ?':
+    return 'I talk.'
+if Question == ''
+    return ''
+
+
+
